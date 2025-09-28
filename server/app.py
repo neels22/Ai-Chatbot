45ba0d184cc4d4d8c4b2bc1e87713382620ae8d5
neels22 <indraneelsarode@gmail.com>
2025-09-28 15:25:39 -0700

    Basic imports Done

---
+++ server/app.py
@@ -0,0 +1,29 @@
+from typing import TypedDict, Annotated, Optional
+from langgraph.graph import add_messages, StateGraph, END
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessageChunk, ToolMessage
+from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+from fastapi import FastAPI, Query
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+import json
+from uuid import uuid4
+from langgraph.checkpoint.memory import MemorySaver
+
+load_dotenv()
+
+# Initialize memory saver for checkpointing
+memory = MemorySaver()
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+search_tool = TavilySearchResults(
+    max_results=4,
+)
+
+tools = [search_tool]
+
+llm = ChatOpenAI(model="gpt-4o")
+
